291644575858960e7a44fb706286ee33653e677d
raf.henry <henryrbco@gmail.com>
2026-03-27 15:41:24 -0300

    Remover e adicionar tarefas

---
+++ to-do-list/script.js
@@ -0,0 +1,34 @@
+const input = document.getElementById("taskInput")
+const button = document.getElementById("addTaskBtn")
+const list = document.getElementById("taskList")
+
+
+button.addEventListener("click", function(){
+
+    const taskText = input.value
+
+    if(taskText === "") return
+
+    const li = document.createElement("li")
+    const remove = document.createElement("button")
+
+    
+    remove.classList.add("remove-btn")
+    remove.textContent = "-"
+
+    remove.addEventListener("click", function(){
+    li.remove()
+})
+
+    li.textContent = taskText
+    
+    li.appendChild(remove)
+    list.appendChild(li)
+
+    input.value = ""
+})
+
+
+
+
+
