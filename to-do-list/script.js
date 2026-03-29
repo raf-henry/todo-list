8421605b4832abdb004a6363791a9d3ecd352230
raf.henry <henryrbco@gmail.com>
2026-03-29 02:18:10 -0300

    marcar tarefas como concluido(salvando) e mostrar quantas tarefas ativas (sem salvar)

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -2,87 +2,146 @@ const input = document.getElementById("taskInput")
 const button = document.getElementById("addTaskBtn")
 const list = document.getElementById("taskList")
 
-function saveTasks() {
+
+
+
+
+function saveTasks(){
+
     const tasks = []
 
-    document.querySelectorAll("#taskList li span").forEach(span => {
-        tasks.push(span.textContent)
+    document.querySelectorAll("#taskList li").forEach(li => {
+
+        const span = li.querySelector("span")
+        const completed = span.classList.contains("completed")
+        
+
+
+        tasks.push({
+
+            text: span.textContent,
+            completed: completed
+        })
+
     })
 
     localStorage.setItem("tasks", JSON.stringify(tasks))
 }
 
-function loadTasks() {
-    const tasks = JSON.parse(localStorage.getItem("tasks")) || []
+function updateCounter(){
+    
+    const tasks = document.querySelectorAll("#taskList li").length
+    if (tasks === 1) {
 
-    tasks.forEach(taskText => {
-        const li = document.createElement("li")
-        const span = document.createElement("span")
-        const remove = document.createElement("button")
+        document.getElementById("taskCounter").textContent =
+        tasks + " tarefa"
 
-        span.textContent = taskText
+    }
 
-        remove.textContent = "-"
-        remove.classList.add("remove-btn")
-
-        remove.addEventListener("click", function() {
-            li.remove()
-            saveTasks() // atualiza o localStorage
-        })
-
-        li.appendChild(span)
-        li.appendChild(remove)
-        list.appendChild(li)
-    })
+    else document.getElementById("taskCounter").textContent =
+         tasks + " tarefas"
 }
 
-function addTask(){
-    const taskText = input.value
-    if(taskText === "") return
+function createTask(taskText, completed = false){
 
     const li = document.createElement("li")
     const span = document.createElement("span")
     const remove = document.createElement("button")
+    const check = document.createElement("button")
 
+    
+
+    span.classList.add("text-align")
     span.textContent = taskText
+
+    check.textContent = "✓"
+    check.classList.add("check-btn")
 
     remove.textContent = "-"
     remove.classList.add("remove-btn")
 
+
+    if(completed){
+        span.classList.add("completed")
+        check.classList.add("completed-btn")
+    }
+
+
     remove.addEventListener("click", function(){
         li.remove()
-        saveTasks() // atualiza ao remover
+        updateCounter()
+        saveTasks()
+        
     })
 
+
+    check.addEventListener("click", function(){
+
+        span.classList.toggle("completed")
+        check.classList.toggle("completed-btn")
+        updateCounter()
+        saveTasks()
+        
+
+    })
+
+    
+    li.appendChild(check)
     li.appendChild(span)
     li.appendChild(remove)
+
     list.appendChild(li)
 
-    input.value = ""
-
-    saveTasks() // atualiza ao adicionar
 }
 
 
-input.addEventListener("keydown", function(event) {
+function addTask(){
 
-    if (event.key === "Enter") {
+    
+
+    const taskText = input.value.trim()
+
+    if(taskText === "") return
+
+    createTask(taskText)
+
+    input.value = ""
+
+    updateCounter()
+    saveTasks()
+
+    
+    
+}
+
+
+function loadTasks(){
+
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || []
+
+    tasks.forEach(task => {
+
+        createTask(task.text, task.completed)
+
+    })
+
+}
+
+
+input.addEventListener("keydown", function(event){
+
+    if(event.key === "Enter"){
 
         event.preventDefault()
-        
+
         addTask()
-        
+
     }
 
 })
 
-button.addEventListener("click", function(){
 
-    addTask()
-    
-})
+button.addEventListener("click", addTask)
 
 
 loadTasks()
-
-
